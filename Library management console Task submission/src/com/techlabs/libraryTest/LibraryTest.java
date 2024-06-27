011c58f2a4dab63764d8ad1f47061090a34ba8b1
Poojitha159 <makkenaharshini@gmail.com>
2024-06-27 17:16:00 +0530

    changed

--- Library management console Task submission/src/com/techlabs/libraryTest/LibraryTest.java
+++ Library management console Task submission/src/com/techlabs/libraryTest/LibraryTest.java
@@ -10,18 +10,25 @@ public class LibraryTest {
 		Book book1 = new Book("1984", "George Orwell");
         User user1 = new User("Alice", "001");
         
-        //userRegistrationManager.registerUser(user1);
-		//user1.registerUser(user1);
-		//user1.borrowBook(book1, user1);
-		//book1.catalogbook(book1, user1);
-		//book1.borrowBook(book1, user1);
-		//book1.returnBook(book1, user1);  
+      
+		user1.registerUser(user1);
+		user1.borrowBook(book1, user1);
+		book1.catalogbook(book1, user1);
+		book1.borrowBook(book1, user1);
+		book1.returnBook(book1, user1);  
+		
+		System.out.println();
 		
 		Book book2=new Book("2013","Sky Dyes");
 		User user2=new User("Bob","002");
+		
 		user2.registerUser(user2);
-		//book2.borrowBook(book2, user1);
+		book2.catalogbook(book2, user2);
+		
+		book2.returnBook(book2, user2);
 		book2.borrowBook(book2, user2);
+		
+		book2.returnBook(book2, user2);
 		
 	}
 	
